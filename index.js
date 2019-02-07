b9ed93d1d39f1cb27cf742ce4ded61c2854e280e
Christina Labay <Christina.m.labay@gmail.com>
2019-02-06 19:58:53 -0600

    add javascript and API functionality

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-THIS IS THE JAVASCRIPT
+'use strict';
+let numImage = 0;
+
+
+
+//retrieve the images
+function getImages(numImage){
+    console.log('inside getImages. numImage ='+ numImage);
+    for(let i = 0; i < numImage; i++){
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(responseJson => 
+                displayImages(responseJson))
+            .catch(error => alert('Whoops! Something went wrong!'));
+    }
+}
+
+//display the images 
+function displayImages(responseJson){
+    const newImg = `<img src="${responseJson.message}">`
+    $('.image-display').append(newImg);
+}
+
+//event listener for the button
+function handleForm(){
+    $('form').submit(event=> {
+        event.preventDefault();
+        $('.image-display').empty();
+        numImage = $('#num-pics').val();
+        if(numImage > 50 || numImage < 1){
+            alert('Valid numbers are 1 through 50');
+        }
+        else{
+            getImages(numImage);
+        }
+    });  
+}
+//when the page loads
+$(function(){
+    console.log('the page is loaded.');
+    handleForm();
+});
